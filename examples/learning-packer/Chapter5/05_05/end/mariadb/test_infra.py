0833fc6567458e7c7105c3146ea51ba7b11a02a0
Maros Kukan <maros.kukan@me.com>
2021-12-15 10:32:39 +0100

    Cleanup old exervice files which used json for template definition

--- examples/learning-packer/Chapter5/05_05/end/mariadb/test_infra.py
+++
@@ -1,9 +0,0 @@
-def test_mariadb_is_installed(host):
-    mariadb = host.package("mariadb-server")
-    assert mariadb.is_installed
-
-def test_mariadb_running_and_enabled(host):
-    mariadb = host.service("mariadb")
-    assert mariadb.is_running
-    assert mariadb.is_enabled
-
