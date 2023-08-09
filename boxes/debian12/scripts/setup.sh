a87d8910065b6e3a057cb5843f9bf42219adcacb
Maros Kukan <maros.kukan@me.com>
2023-08-08 23:27:47 -0700

    fix: remove workaround for vbox

--- boxes/debian12/scripts/setup.sh
+++ boxes/debian12/scripts/setup.sh
@@ -3,16 +3,5 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-# Workaround for vbox gray box
-# https://forums.debian.net/viewtopic.php?t=155301
-if [ "$PACKER_BUILDER_TYPE" = "virtualbox-iso" ]; then
-    sed -i 's/bullseye/bookworm/g' /etc/apt/sources.list;
-    apt-get -y update;
-    apt-get -y upgrade --without-new-pkgs;
-    apt-get -y full-upgrade
-    systemctl reboot
-
-fi
-
 # Upgrade all installed packages
 apt-get -y upgrade;
