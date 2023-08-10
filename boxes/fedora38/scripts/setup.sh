8cbaf9231e029757913b81785f73dcc58c47eadf
Maros Kukan <maros.kukan@me.com>
2023-08-10 07:01:41 -0700

    feat: vmware_desktop support

--- boxes/fedora38/scripts/setup.sh
+++ boxes/fedora38/scripts/setup.sh
@@ -7,8 +7,10 @@ HYPERVISOR=`dmesg | grep "Hypervisor detected" | awk -F': ' '{print $2}'`
 
 if [ "$HYPERVISOR" = "Microsoft Hyper-V" ]; then
   printf "Microsoft Hyper-V Detected.\n"
-elif [ "$HYPERVISOR" = "vmware-iso" ]; then
+elif [ "$HYPERVISOR" = "VMware" ]; then
   printf "VMware Workstation Detected.\n"
+  dnf install -y open-vm-tools
+  systemctl enable --now vmtoolsd
 elif [ "$HYPERVISOR" = "virtualbox-iso" ]; then
   printf "Oracle VirtualBox Detected.\n"
   # Install tooling required for virtualization kernel modules
